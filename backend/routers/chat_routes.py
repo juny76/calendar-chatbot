79405f32eca41decc34d7a5a899e6ea097d8dd18
juny76 <tuannguyenhuy87@gmail.com>
2024-01-31 00:01:53 +0700

    fix syntax error

--- backend/routers/chat_routes.py
+++ backend/routers/chat_routes.py
@@ -1,11 +1,7 @@
 from fastapi import APIRouter, HTTPException
-<<<<<<< HEAD
-from backend.llm_agent import run_agent_executor
-=======
 
 from backend.dependencies import get_db
-from backend.usecases import run_agent_executor
->>>>>>> 6640f2335eef9a704365f9164ece96e9f6a99663
+from backend.llm_agent import run_agent_executor
 from backend.models import ChatRequest,ChatResponse
 
 router = APIRouter()
